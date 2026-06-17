f0911740a8b91bae36762125f8d19a1623fdfb0f
Abdul Basim <abdulbasim393@gmail.com>
2026-06-17 11:28:27 +0530

    Created Models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+
+    sku = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    barcode = models.CharField(
+        max_length=100,
+        unique=True,
+        blank=True
+    )
+
+    description = models.TextField(
+        blank=True
+    )
+
+    cost_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    selling_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=0
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.name
